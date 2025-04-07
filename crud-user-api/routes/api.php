14c4e8886ad618d7b3936479459a98dd7d17c4b2
Rizki-Rahmadani <rzkrhmdn7@gmail.com>
2025-04-07 16:06:00 +0700

    feat: Get user by id

--- crud-user-api/routes/api.php
+++ crud-user-api/routes/api.php
@@ -7,4 +7,5 @@ use App\Http\Controllers\UserController;
 Route::middleware('api')->group(function () {
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{id}', [UserController::class, 'show']);
 });
